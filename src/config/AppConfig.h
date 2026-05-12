93081042e5a8c32a1e4bae20415196cc951c97bc
Bach N <login@xbach.cz>
2026-05-12 12:28:11 +0200

    chore: bump FIRMWARE_RELEASE to 6

--- src/config/AppConfig.h
+++ src/config/AppConfig.h
@@ -7,7 +7,7 @@
 // ============================================================================
 // Firmware Version
 // ============================================================================
-#define FIRMWARE_RELEASE "5"
+#define FIRMWARE_RELEASE "6"
 
 // Build ID is injected by build script (8 hex characters)
 // Generated from build timestamp using DJB2 hash algorithm
